7c81dc71ff8f2177347f4e736cfe3e6279d017e3
nick van oostrom <nick.v.oostrom@gmail.com>
2020-05-14 11:13:36 +0200

    facicon plus alle logo's + bootstrap tamplate en js er bij

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./icon/logofavicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./css/all.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <title>technostuff</title>
+
+
+</head>
+<body>
+    hello world
+
+
+
+
+
+    
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</body>
+</html>
