d4713e2bcacf6ea06dd33af82b599130aac3eed9
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-06-04 10:37:00 +0200

    home page vorm staat

--- pagina/home.php
+++ pagina/home.php
@@ -3,6 +3,14 @@
         <div class="col-6">
             <img src="./img/afbeelding-home.PNG" alt="">
         </div>
-        <div class="col-6">welkom text</div>
+        <div class="col-6">
+            <h1>Welkomstwoord</h1>
+            <p>Beste bezoeker,</p>
+            <p>Van harte welkom op de webshop van technostuff. Hier kun je terecht voor laptops,telefoons,smartwatches en nog veel meer. 
+            </p>
+            <p>we hopen dat u alles kan vinden wat u nodig hebt</p>
+            <p>met vriendelijke groet</p>
+            <p>team technostuff</p>
+        </div>
     </div>
 </div>
