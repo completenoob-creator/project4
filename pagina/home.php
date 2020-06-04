bcddfe8c975a82883735772f4e288a7b3f3a5333
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-06-04 10:09:19 +0200

    begonnen met home pagina

--- pagina/home.php
+++ pagina/home.php
@@ -1,3 +1,8 @@
-
-<div class="col-6">plaatje</div>
-<div class="col-6">welkom text</div>
+<div class="home-opbouw">
+    <div class="row">
+        <div class="col-6">
+            <img src="./img/afbeelding-home.PNG" alt="">
+        </div>
+        <div class="col-6">welkom text</div>
+    </div>
+</div>
