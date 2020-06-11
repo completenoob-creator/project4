6d68a67d3efc886f031d29a0822def4066c66280
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-06-11 09:44:14 +0200

    bug fixed registreren knop

--- pagina/inloggen.php
+++ pagina/inloggen.php
@@ -12,6 +12,7 @@
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary" onclick="registreren()">account maaken</button>
   <button type="submit" class="btn btn-primary" onclick="o()">login</button>
 </form>
+
+<button type="submit" class="btn btn-primary" onclick="registreren()">account maaken</button>
