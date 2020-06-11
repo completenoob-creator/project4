d64b9e78001ee26d87adffc20657bc2f1a0708a4
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-06-11 11:52:10 +0200

    content fout gefixed

--- pagina-s/content.php
+++ pagina-s/content.php
@@ -1,15 +1,19 @@
 <?php
         if (isset($_GET['content'])) {
-            if (strpos($_GET['content'], 'script') === false) {
-                $filename = 'pagina/' . $_GET['content'] . '.php';
-            } else {
-                $filename = 'scripts/' . $_GET['content'] . '.php';
-            }
-
             if (file_exists('pagina/' . $_GET['content'] . '.php')) {
                 include 'pagina/' . $_GET['content'] . '.php';
             } 
-        } else {
+            elseif(file_exists('scripts/' . $_GET['content'] . '.php')){
+                include 'scripts/' . $_GET['content'] . '.php';
+            } else {
             include 'pagina/home.php';
         }
+        }
+           
     ?>
+<!-- 
+if (strpos($_GET['content'], 'script') === false) {
+                $filename = 'pagina/' . $_GET['content'] . '.php';
+            } else {
+                $filename = 'scripts/' . $_GET['content'] . '.php';
+            } -->
