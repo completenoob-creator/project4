b9bfe3d088e107e4966aef4f3cffcbf41926c45b
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-06-18 13:40:09 +0200

    bug function_script

--- pagina/producten1.php
+++ pagina/producten1.php
@@ -1,37 +1,61 @@
+<?php
+
+
+?>
+
+
 <div class="rijen-webshop">
     <div class="row">
+    
         <div class="col-4">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="./index.php?content=winkelwagen"><img src="./img/wagen.png" height="30px" alt=""></a>
+            <form>
+                <div class="card-shadow" style="width: 18rem;" >
+                    <div>
+                        <img src="./img/laptop1.jpg" height="10px" alt="" class="img-fluid card-img-top">
+                    </div>
+                
+                    <div class="card-body">
+                        <h5 class="card-title">product1</h5>
+                        <h6>
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        </h6>
+                        <p class="card-text">
+                            some quick example text to build on the card
+                        </p>
+                        <small><s class="text-secondary">$519</s></small>
+                        <span class="price">$599</span>
+                        <button tyoe="submit" name="add" class="btn btn-warning my-3">add to cart <img src="./img/wagen.png" height="10px" alt=""></button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
         <div class="col-4">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="./index.php?content=winkelwagen"><img src="./img/wagen.png" height="30px" alt=""></a>
+            <form>
+                <div class="card-shadow" style="width: 18rem;" >
+                    <div>
+                        <img src="./img/laptop1.jpg" height="10px" alt="" class="img-fluid card-img-top">
+                    </div>
+                
+                    <div class="card-body">
+                        <h5 class="card-title">product1</h5>
+                        <h6>
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        <img src="./img/wagen.png" height="10px" alt="">
+                        </h6>
+                        <p class="card-text">
+                            some quick example text to build on the card
+                        </p>
+                        <small><s class="text-secondary">$519</s></small>
+                        <span class="price">$599</span>
+                        <button tyoe="submit" name="add" class="btn btn-warning my-3">add to cart <img src="./img/wagen.png" height="10px" alt=""></button>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="./index.php?content=winkelwagen"><img src="./img/wagen.png" height="30px" alt=""></a>
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
--- scripts/functions.php
+++ scripts/functions.php
@@ -27,4 +27,34 @@ function sanitize($raw_data){
                  "date" => $date_formated,
                  "time" => $time_formated);
  };
+
+ function component(){
+    $element='
+   <div class="col-4">
+   <form>
+       <div class="card-shadow" style="width: 18rem;" >
+           <div>
+               <img src="./img/laptop1.jpg" height="10px" alt="" class="img-fluid card-img-top">
+           </div>
+       
+           <div class="card-body">
+               <h5 class="card-title">product1</h5>
+               <h6>
+               <img src="./img/wagen.png" height="10px" alt="">
+               <img src="./img/wagen.png" height="10px" alt="">
+               <img src="./img/wagen.png" height="10px" alt="">
+               <img src="./img/wagen.png" height="10px" alt="">
+               </h6>
+               <p class="card-text">
+                   some quick example text to build on the card
+               </p>
+               <small><s class="text-secondary">$519</s></small>
+               <span class="price">$599</span>
+               <button tyoe="submit" name="add" class="btn btn-warning my-3">add to cart <img src="./img/wagen.png" height="10px" alt=""></button>
+           </div>
+       </div>
+   </form>
+</div>
+ '};
+
 ?>
