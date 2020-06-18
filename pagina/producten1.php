df4aee55d4d0e96f41815bb0a16a07007211c0f6
nick van oostrom <nick.v.oostrom@gmail.com>
2020-06-18 17:01:14 +0200

    error pull fail

--- pagina/producten1.php
+++ pagina/producten1.php
@@ -7,9 +7,10 @@ require_once("./scripts/functions.php");
 <div class="rijen-webshop">
     <div class="row">
    <?php 
-   component("product 1");
-   component($naam);
-   component($naam);
+   component("product1", 599 ,"./img/laptop1");
+   component("product2", 750 , "./img/laptop1");
+   component("product3", 720 , "./img/laptop1");
    ?> 
+        
     </div>
 </div>
