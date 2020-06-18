ffdce730ec11c3b13d25b0d4b583572af782a783
nick van oostrom <nick.v.oostrom@gmail.com>
2020-06-18 16:13:36 +0200

    error?

--- pagina/producten1.php
+++ pagina/producten1.php
@@ -6,10 +6,9 @@ require_once("./scripts/functions.php");
 <div class="rijen-webshop">
     <div class="row">
    <?php 
-   component("product1", 599 ,"./img/laptop1");
-   component("product2", 750 , "./img/laptop1");
-   component("product3", 720 , "./img/laptop1");
+   component("product 1");
+   component($naam);
+   component($naam);
    ?> 
-        
     </div>
 </div>
