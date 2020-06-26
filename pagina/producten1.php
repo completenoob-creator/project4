d9c29430a2840633a07d0caa02c8d7816dcb8af4
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-06-26 10:09:46 +0200

    feat carts

--- pagina/producten1.php
+++ pagina/producten1.php
@@ -1,17 +1,25 @@
 <?php
-require_once("./scripts/connect_db.php");
-require_once("./scripts/functions.php");
+require_once("./scripts/product_db.php");
+require_once("./scripts/compononent.php");
 
+   
+        if (isset($_POST['add'])){
+            // print_r($_POST['product_id']);
+            if (isset($_SESSION['cart'])){
+                $item_array_id= array_column($_SESSION['cart'],'product_id');
+                
+            }else{
+                $item_array = array(
+                    'product_id' => $_POST['product_id']
+                );
+
+                $_SESSION['cart'][0] = $item_array;
+
+            }
+        }
+
+    $result = getData();
+    while($row = mysqli_fetch_assoc($result)){;
+    component($row['product_name'], $row['product_price'], $row['total_price'],$row['product_img'],$row['id']);
+   }
 ?>
-
-
-<div class="rijen-webshop">
-    <div class="row">
-   <?php 
-   component("product1", 599 ,"./img/laptop1");
-   component("product2", 750 , "./img/laptop1");
-   component("product3", 720 , "./img/laptop1");
-   ?> 
-        
-    </div>
-</div>
---
+++ scripts/compononent.php
@@ -0,0 +1,40 @@
+<?php
+require_once("./pagina/producten1.php");
+require_once("./scripts/product_db.php");
+
+?>
+<div class="row">
+<?php
+function component($product_name,$product_price,$total_price,$productimg,$product_id){
+    $element="<div class='col-6'>
+    <form action='./index.php?content=producten1' method='POST'>
+        <div class='card-shadow' style='width: 18rem;' >
+            <div>
+                <img src='$productimg' height='10px' alt='' class='img-fluid card-img-top'>
+            </div>
+        
+            <div class='card-body'>
+                <h5 class='card-title'>$product_name</h5>
+                <h6>
+                <img src='./img/wagen.png' height='10px' alt=''>
+                <img src='./img/wagen.png' height='10px' alt=''>
+                <img src='./img/wagen.png' height='10px' alt=''>
+                <img src='./img/wagen.png' height='10px' alt=''>
+                </h6>
+                <p class='card-text'>
+                    some quick example text to build on the card
+                </p>
+                <span class='price'> $$product_price</span>
+                <span class='price'> inclusief btw $$total_price</span>
+                <button tyoe='submit' name='add' class='btn btn-warning my-3'>add to cart <img src='./img/wagen.png' height='10px' alt=''></button>
+                <input type='hidden' name='product_id' value='$product_id'>
+                 </div>
+        </div>
+    </form>
+ </div>";
+ echo $element;
+
+ }
+ 
+ ?>
+</div>
